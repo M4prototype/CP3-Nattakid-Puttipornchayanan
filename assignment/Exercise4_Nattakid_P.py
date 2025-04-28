a0d8d1cc6178d0529df53b57830a2d62fd32aeed
M4prototype <tianzaza03@gmail.com>
2025-04-28 16:35:05 +0700

    Create Exercise4_Nattakid_P.py

---
+++ assignment/Exercise4_Nattakid_P.py
@@ -0,0 +1,9 @@
+FE = 85.6
+GB = 73.5
+ITCS = 99.9
+CP = 89.9
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ITCS)
+print("Computer Programming : ",CP)
