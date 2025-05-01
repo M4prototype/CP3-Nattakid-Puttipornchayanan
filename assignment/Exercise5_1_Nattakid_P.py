55cc4b5e847c4d948de82f4f3996373ea8638210
M4prototype <tianzaza03@gmail.com>
2025-05-01 11:06:50 +0700

    Create Exercise5_1_Nattakid_P.py

---
+++ assignment/Exercise5_1_Nattakid_P.py
@@ -0,0 +1,6 @@
+num1=int(input("FirstNumber : "))
+num2=int(input("SecondNumber : "))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
